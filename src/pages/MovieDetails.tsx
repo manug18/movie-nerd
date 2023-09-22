b272828a7984abe4e628b3cd25abdb103c4593ae
manug18 <manasgoyal913@gmail.com>
2023-09-22 15:34:18 +0530

    correct-display-of-backdrop

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,20 +1,30 @@
-import { Card, CardMedia, Stack } from "@mui/material";
+import { Card, CardMedia, Skeleton, Stack, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getMovieById } from "../services/ApiRequest";
 import { colors } from "../styles/Colors";
 import HomePageFooter from "../components/Footer";
+import { useEffect, useState } from "react";
+import { MovieDetails } from "../models/UpcomingMovie";
 
-const imageBaseUrl = "https://image.tmdb.org/t/p/original"; // Replace with your image base URL
+const imageBaseUrl = "https://image.tmdb.org/t/p/original";
 
-export default function MovieDetails() {
+export default function MovieDetailedInformation() {
   const { movieId } = useParams();
-  const parsedMovieId = movieId ? parseInt(movieId, 10) : 0; // Parse to number or leave it as undefined
+
+  const parsedMovieId = movieId ? parseInt(movieId, 10) : 0;
   const movieService = useQuery({
     queryKey: ["get-movie"],
-    queryFn: () => getMovieById({ movieId: parsedMovieId }), // Pass the parsed movieId
+    queryFn: () => getMovieById({ movieId: parsedMovieId }),
     enabled: !!parsedMovieId,
   });
+  const [movieDetails, setMovieDetails] = useState<MovieDetails>();
+  useEffect(() => {
+    if (movieService.data) {
+      setMovieDetails(movieService.data);
+    }
+  }, [movieService.data]);
+
   const backdropPath = movieService.data?.backdrop_path;
   const imageUrl = `https://image.tmdb.org/t/p/original${movieService.data?.poster_path}`;
 
@@ -28,27 +38,63 @@ export default function MovieDetails() {
             : "none",
           backgroundSize: "cover",
           backgroundPosition: "center",
-
-          bgcolor: "rgba(100, 0, 255, 0.2)", // Adjust the color and opacity here
           height: "100vh",
-          position: "relative",
         }}
         spacing={2}
       >
         <Stack
+          direction={"row"}
           sx={{
-            position: "relative",
             zIndex: 1,
-            textAlign: "center",
-            padding: "2rem",
-            opacity: 1,
+            bgcolor: colors.primary,
+            opacity: 0.9,
+            height: "100%",
           }}
         >
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia component="img" image={imageUrl} alt="green iguana" />
-          </Card>
+          <Stack sx={{ width: "30%", py: "10%", pl: "10%", pr: "5%" }}>
+            <Card
+              sx={{
+                borderRadius: 7, // Add border radius to the Card
+                display: "flex", // Use flex display to align image and content
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={imageUrl}
+                alt="Movie Poster"
+                sx={{
+                  height: "100%", // Make the image fill the Card vertically
+                  objectFit: "cover", // Ensure the image covers the Card
+                  borderTopLeftRadius: 7, // Add border radius to the top-left corner
+                  borderBottomLeftRadius: 10, // Add border radius to the bottom-left corner
+                }}
+              />
+            </Card>
+          </Stack>
+          <Stack sx={{ width: "70%", py: "10%" }}>
+            <Typography color={colors.white} fontSize={26}>
+              {movieDetails?.original_title}(
+              {movieDetails?.release_date.slice(0, 4)})
+            </Typography>
+            <Typography color={colors.grey} fontStyle="oblique">
+              {movieDetails?.tagline}
+            </Typography>
+            <Stack direction={"row"} p={1} m={1}>
+              <Typography color={colors.white}>Status:</Typography>
+              <Typography color={colors.grey}>
+                {movieDetails?.status}
+              </Typography>
+              <Typography color={colors.white}>Release Date:</Typography>
+              <Typography color={colors.grey}>
+                {movieDetails?.release_date}
+              </Typography>
+              <Typography color={colors.white}>Length:</Typography>
+              <Typography color={colors.grey}>
+                {movieDetails?.runtime}
+              </Typography>
+            </Stack>
+          </Stack>
         </Stack>
-        hello {movieService.data?.budget}
       </Stack>
       <HomePageFooter />
     </>
--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -2,5 +2,6 @@ export const colors ={
     footer:'#020C1B',
     primary:'#04152D',
     white:"#FFFFFF",
-    buttonHide:"#683614"
+    buttonHide:"#683614",
+    grey:'#828A95'
 }
